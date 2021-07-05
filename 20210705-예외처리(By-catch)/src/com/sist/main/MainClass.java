dc27b667302da4707dab005486bc5f8b3e67b330
sist <sist@DESKTOP-LVE6G7J>
2021-07-05 15:14:48 +0900

    11

---
+++ 20210705-예외처리(By-catch)/src/com/sist/main/MainClass.java
@@ -0,0 +1,19 @@
+package com.sist.main;
+// 패키지가 다르면 import를 사용한다
+import com.sist.dao.Emp;
+import com.sist.dao.EmpDAO;
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EmpDAO dao=new EmpDAO();
+		Emp[] emp=dao.empAllData(); // => 브라우저
+		for(Emp e:emp) {
+			System.out.println(e.getEmpno()+" "
+					 +e.getEname()+" "
+					 +e.getJob()+" "
+					 +e.getSal()+" ");
+		}
+	}
+
+}
